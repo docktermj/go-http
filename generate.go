768b8f859eee0a327e7e2ca8ed03e217b2e8a596
docktermj <michael@dockter.com>
2023-05-21 23:19:54 -0400

    #1 Savepoint

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
-package project
+package generate
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target senzing --debug.noerr --clean senzing-rest-api.yaml
+// For ogen options, see https://github.com/ogen-go/ogen/blob/main/cmd/ogen/main.go
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target senzinghttpapi --package senzinghttpapi  --generate-tests --debug.noerr --clean senzing-rest-api.yaml
